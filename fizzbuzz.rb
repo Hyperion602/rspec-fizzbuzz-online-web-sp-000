a4606e17972b73bcab812a5bd7e5127561707e86
James Harris <mharris9608@yahoo.com>
2020-12-02 18:51:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-  "FizzBuzz"
+    "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0
+    "Fizz"
   else
   end
 end
